8bcdec6d80480ee5ba59070a6d9a058627eb993e
Rushil <rushil.jivan@sambeconsulting.com>
2026-04-02 01:26:10 +0200

    Commit python files

---
+++ Python for Beginners/Python Testing/data types.py
@@ -0,0 +1,38 @@
+# integer
+integer = 100
+print(integer)
+
+# print the type of variable
+print(type(integer))
+
+# boolean
+boolean = True
+print(boolean)
+
+# float
+float = 3.14
+print(float)
+
+# imaginary number
+imaginary = 10j
+print(imaginary)
+
+# string
+text = "hello bob"
+print(text)
+
+# list, sequence
+numbers = [1, 2, 3]
+print(numbers)
+
+# sequence
+coordinates = (2.5, 10)
+print(coordinates)
+
+# mapping types
+users = {'mario': 1, 'luigi': 2}
+print(users)
+
+# set types
+raffle = {1, 10, 25, 50}
+print(raffle)
---
+++ Python for Beginners/PythonProject/main.py
@@ -0,0 +1,20 @@
+print("Hello bob") #comment
+
+print("Hello" + " Bob")
+print("Hello" + " Cat")
+print("Hello" + " Dave")
+
+greeting = "Hello"
+
+print(greeting + " Bob")
+print(greeting + " Cat")
+print(greeting + " Dave")
+
+PI = 3.14
+VERSION = 2
+
+PI = 4
+
+print(PI)
+print(VERSION)
+
