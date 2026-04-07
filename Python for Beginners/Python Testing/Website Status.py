df1344befc08763999e31288f702756da9a70de3
Rushil <rushil.jivan@sambeconsulting.com>
2026-04-07 10:56:54 +0200

    Commit project 07/04

---
+++ Python for Beginners/Python Testing/Website Status.py
@@ -0,0 +1,26 @@
+import requests
+from requests import Response
+
+def get_response(url: str) -> Response:
+    return requests.get(url)
+
+def show_response_info(response: Response) -> None:
+    print('Status:', response.status_code)
+    print('Ok:', response.ok)
+    print('Links:', response.links)
+    print('Encoding:', response.encoding)
+    print('Is redirect:', response.is_redirect)
+    print('is permanent redirect:', response.is_permanent_redirect)
+
+def main() -> None:
+    website: str = "https://www.idently.io"
+    response: Response = get_response(website)
+    show_response_info(response)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
