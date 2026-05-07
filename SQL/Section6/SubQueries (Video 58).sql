5648d934e09bb89168c0aa19c4fcbd2e08024263
Rushil <rushil.jivan@sambeconsulting.com>
2026-05-07 15:35:48 +0200

    SQL scripts 07/05

---
+++ SQL/Section6/SubQueries (Video 58).sql
@@ -0,0 +1,35 @@
+
+
+
+ ------------------------------------------------------------------------------
+
+
+  select * from [Production].[Product]
+
+-------------------------------------------------------------------------------------
+
+  select * from [Production].[ProductInventory]
+
+-- ALL TABLES GET CREATED IN DBO SCHEMA UNLESS WE CREATE A NEW SCHEMA
+
+-------------------------------------------------------------------------------------
+
+
+SELECT * FROM dbo.EMPLOYEE
+
+-----------------------------------
+
+SELECT *
+  FROM [HumanResources].[EmployeePayHistory]
+  WHERE [BusinessEntityID] IN (SELECT BusinessEntityID FROM [HumanResources].[EmployeePayHistory] WHERE Rate > 60)
+
+----------------------------------------
+
+SELECT *
+  FROM [HumanResources].[EmployeePayHistory]
+  WHERE [BusinessEntityID] IN (SELECT BusinessEntityID FROM [HumanResources].[EmployeePayHistory] WHERE Rate = 39.06)
+
+---------------------------------------------
+
+ select * from [Production].[Product]
+ where ProductID in ( select ProductID from [Production].[ProductInventory] where Quantity > 300)
