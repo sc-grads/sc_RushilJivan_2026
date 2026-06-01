8e0ffb2deb9c4c16150cc13a57f0169f8e6ad2be
Rushil <rushil.jivan@sambeconsulting.com>
2026-06-01 15:57:55 +0200

    Commit sql scripts for 01/06

---
+++ Database Administration/T SQL/Section 21/124 - Create first procedure.sql
@@ -0,0 +1,12 @@
+create proc NameEmployees as
+begin
+	select EmployeeNumber, EmployeeFirstName, EmployeeLastName
+	from tblEmployee
+end
+go
+
+NameEmployees
+
+execute NameEmployees
+
+exec NameEmployees
